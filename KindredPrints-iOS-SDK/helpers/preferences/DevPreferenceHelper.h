687627b0ccd706fadde19f57dc0b4121fab11b0c
Alex Austin <alexjaustin@gmail.com>
2014-04-21 14:34:41 -0700

    Point to live server

--- KindredPrints-iOS-SDK/helpers/preferences/DevPreferenceHelper.h
+++ KindredPrints-iOS-SDK/helpers/preferences/DevPreferenceHelper.h
@@ -10,7 +10,7 @@
 #import "PreferenceHelper.h"
 
 static BOOL LOG = YES;
-static BOOL DEV = YES;
+static BOOL DEV = NO;
 
 @interface DevPreferenceHelper : PreferenceHelper
 + (BOOL)testForNullValue:(id)object;
